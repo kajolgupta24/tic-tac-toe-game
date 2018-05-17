68b28f17cd6d7a9132194113039fb42c21aa73bc
Kajol Gupta <kajolgupta1811@gmail.com>
2018-05-17 15:14:35 +0530

    small change in next player for index.js

--- my-react-game/src/index.js
+++ my-react-game/src/index.js
@@ -46,7 +46,7 @@ function Square(props) {
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
      return (
         <div>
